4ad362b8fe20ef4361c46ae4af445724a893a8d7
Naufra1 <gustavoresende3e@gmail.com>
2024-03-27 16:10:27 -0300

    feat: add ShowComputers logic

--- handler/showComputers.go
+++ handler/showComputers.go
@@ -3,11 +3,16 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Naufra1/ByteApi/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowComputersHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "Success",
-	})
+	computers := []schemas.Computer{}
+
+	if err := db.Find(&computers).Error; err != nil {
+		SendError(ctx, http.StatusInternalServerError, "erro ao listar computadores")
+		return
+	}
+	SendSuccess(ctx, "list-computers", computers)
 }
